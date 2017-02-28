cece3130334eae8916229b964cac1d0ad2bf273f
BrianLusina <lusinabrian@gmail.com>
2017-02-28 18:59:45 +0300

    create application factory object
    this will allow creationg of flask app instances that will enable for use in testing, production and development

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask
-from app.manage.views import manage
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_script import Manager
+from config import config
+
+db = SQLAlchemy()
 
 
 def create_app(config_name):
@@ -12,17 +12,47 @@ def create_app(config_name):
     :return: a new WSGI flask application
     :rtype: object
     """
+    app = Flask(__name__, template_folder="templates", static_folder="static")
+    # app configurations, considering config is a dictionary, we pass in the key that we will receive
+    app.config.from_object(config[config_name])
 
-app = Flask(__name__)
-app.debug = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    # initialize the db
+    db.init_app(app)
 
-db = SQLAlchemy(app)
+    # register error pages and blueprints
+    # error_handlers(app)
+    register_blueprints(app)
 
-migrate = Migrate(app, db)
-manager = Manager(app)
+    # finally return the new flask application
+    return app
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.register_blueprint(manage)
+# todo: implement your error handlers here
+# def error_handlers(app):
+#     """
+#     Function that will handle the erros encountered in the application
+#     :param app: Application object
+#     """
+#
+#     # Error handler for page not found
+#     @app.errorhandler(404)
+#     def not_found():
+#         return render_template('errorpages/404.html')
+#
+#     @app.errorhandler(403)
+#     def error_403():
+#         return render_template("errorpages/403.html")
+#
+#     @app.errorhandler(403)
+#     def error_500():
+#         return render_template("errorpages/500.html")
+#
+#     @app.errorhandler(400)
+#     def not_found():
+#         return render_template('errorpages/400.html')
 
+
+def register_blueprints(app):
+    from app.manage.views import manage
+
+    app.register_blueprint(manage)
